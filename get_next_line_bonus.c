24d7fe5387906ab16e833e0af803eea93914bc42
Beyza Nur Dalmaz <bdalmaz@k1m16s01.42istanbul.com.tr>
2025-09-09 17:52:20 +0300

    bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdalmaz <bdalmaz@student.42.fr>            #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-09-08 17:35:25 by bdalmaz           #+#    #+#             */
+/*   Updated: 2025-09-08 17:35:25 by bdalmaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_read_file(int fd, char *buffer)
+{
+	char	*temp;
+	int		readd;
+
+	if (!buffer || !*buffer)
+		buffer = ft_strdup("");
+	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	temp[0] = '\0';
+	readd = 1;
+	while (readd > 0 && !ft_strchr(temp, '\n'))
+	{
+		readd = read(fd, temp, BUFFER_SIZE);
+		if (readd < 0)
+			return (free(temp), free(buffer), NULL);
+		temp[readd] = '\0';
+		buffer = ft_strjoin(buffer, temp);
+	}
+	if (temp)
+		free(temp);
+	return (buffer);
+}
+
+char	*ft_get_line(char *buffer)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	temp = ft_substr(buffer, 0, i);
+	return (temp);
+}
+
+char	*ft_next(char *buffer)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	temp = ft_substr(buffer, i, ft_strlen(buffer) - i);
+	free(buffer);
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[1024];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer[fd] = ft_read_file(fd, buffer[fd]);
+	if (!buffer[fd])
+		return (NULL);
+	line = ft_get_line(buffer[fd]);
+	buffer[fd] = ft_next(buffer[fd]);
+	return (line);
+}
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bdalmaz <bdalmaz@student.42.fr>            #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
